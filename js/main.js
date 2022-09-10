84464533b2f53b834ed4a1aacdd2f5149a61e409
Andres Gonzalez <a_gonzalez214@hotmail.com>
2022-09-10 11:14:12 -0300

    Primer Intento Web

---
+++ js/main.js
@@ -0,0 +1,94 @@
+//nav fixed on top
+const main = document.querySelector('#nav-scroll')
+const nav = document.querySelector('#nav')
+
+let offset = main.offsetHeight - nav.offsetHeight
+
+window.onscroll = function () {
+  if (window.scrollY > offset) {
+    nav.classList.remove('bottom-nav')
+    nav.classList.add('top-nav')
+  } else {
+    nav.classList.add('bottom-nav')
+    nav.classList.remove('top-nav')
+  }
+}
+
+const logo = document.querySelector('main-logo')
+
+// dia actual
+
+let lunes = document.querySelectorAll('.lunes')
+let martes = document.querySelectorAll('.martes')
+let jueves = document.querySelectorAll('.jueves')
+let viernes = document.querySelectorAll('.viernes')
+let sabado = document.querySelectorAll('.sabado')
+
+let miercoles = document.querySelectorAll('.miercoles')
+
+window.onload =  function() {
+
+  switch (new Date().getDay()) {
+
+    case 1:
+      Array.from(lunes).forEach(element => element.classList.add('dia-actual'))
+      Array.from(martes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(miercoles).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(jueves).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(viernes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(sabado).forEach(element => element.classList.remove('dia-actual'))
+      break
+
+    case 2:
+         Array.from(lunes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(martes).forEach(element => element.classList.add('dia-actual'))
+      Array.from(miercoles).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(jueves).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(viernes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(sabado).forEach(element => element.classList.remove('dia-actual'))('dia-actual')
+      break
+    case 3:
+         Array.from(lunes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(martes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(miercoles).forEach(element => element.classList.add('dia-actual'))
+      Array.from(jueves).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(viernes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(sabado).forEach(element => element.classList.remove('dia-actual'))('dia-actual')
+      break
+      case 4:
+            Array.from(lunes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(martes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(miercoles).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(jueves).forEach(element => element.classList.add('dia-actual'))
+      Array.from(viernes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(sabado).forEach(element => element.classList.remove('dia-actual'))('dia-actual')
+      break
+        break
+
+    case 5:
+         Array.from(lunes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(martes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(miercoles).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(jueves).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(viernes).forEach(element => element.classList.add('dia-actual'))
+      Array.from(sabado).forEach(element => element.classList.remove('dia-actual'))('dia-actual')
+      break
+
+    case 6:
+         Array.from(lunes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(martes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(miercoles).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(jueves).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(viernes).forEach(element => element.classList.remove('dia-actual'))
+      Array.from(sabado).forEach(element => element.classList.add('dia-actual'))('dia-actual')
+      break
+      case 0:
+        Array.from(lunes).forEach(element => element.classList.remove('dia-actual'))
+     Array.from(martes).forEach(element => element.classList.remove('dia-actual'))
+     Array.from(miercoles).forEach(element => element.classList.remove('dia-actual'))
+     Array.from(jueves).forEach(element => element.classList.remove('dia-actual'))
+     Array.from(viernes).forEach(element => element.classList.remove('dia-actual'))
+     Array.from(sabado).forEach(element => element.classList.remove('dia-actual'))('dia-actual')
+     break
+  }
+}
